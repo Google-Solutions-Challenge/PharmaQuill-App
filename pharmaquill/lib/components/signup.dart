3572281e6b5f2579c7340938358a40d2232231ef
Madhurjya Dasgupta <madhurjya2003@icloud.com>
2024-02-16 18:52:40 +0000

    Added gradient borders and icons

--- pharmaquill/lib/components/signup.dart
+++ pharmaquill/lib/components/signup.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gradient_borders/gradient_borders.dart';
 
 class SignUp extends StatefulWidget {
   const SignUp({Key? key}) : super(key: key);
@@ -37,14 +38,6 @@ class _SignUpState extends State<SignUp> {
                           Color.fromARGB(255, 95, 128, 236),
                         ],
                       ),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.white,
-                          offset: Offset(0.0, 0.0),
-                          blurRadius: 0.0,
-                          spreadRadius: 0.0,
-                        ),
-                      ],
                     ),
                   ),
                 ])),
@@ -56,23 +49,23 @@ class _SignUpState extends State<SignUp> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Expanded(
-                      child: _buildTextField("Username"),
+                      child: _buildTextField("Username", Icons.person),
                     ),
                     const SizedBox(height: 10),
                     Expanded(
-                      child: _buildTextField("DOB"),
+                      child: _buildTextField("DOB", Icons.calendar_month),
                     ),
                     const SizedBox(height: 10),
                     Expanded(
-                      child: _buildTextField("Gender"),
+                      child: _buildTextField("Gender", Icons.family_restroom),
                     ),
                     const SizedBox(height: 10),
                     Expanded(
-                      child: _buildTextField("Email"),
+                      child: _buildTextField("Email", Icons.email),
                     ),
                     const SizedBox(height: 10),
                     Expanded(
-                      child: _buildTextField("Password"),
+                      child: _buildTextField("Password", Icons.password),
                     ),
                   ],
                 ),
@@ -82,20 +75,24 @@ class _SignUpState extends State<SignUp> {
         ));
   }
 
-  Widget _buildTextField(String hintText) {
+  Widget _buildTextField(String hintText, IconData icon) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
       child: TextField(
         decoration: InputDecoration(
-          hintText: hintText,
-          prefixIcon: const Icon(Icons.person),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(30),
-            borderSide: const BorderSide(color: Colors.blue, width: 2.0),
-          ),
-          filled: true,
-          fillColor: Colors.white.withOpacity(0.3),
-        ),
+            hintText: hintText,
+            prefixIcon: Icon(icon),
+            border: const GradientOutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(30)),
+                width: 3,
+                gradient: LinearGradient(colors: [
+                  Color.fromARGB(255, 11, 30, 110),
+                  Color.fromARGB(255, 63, 88, 176),
+                  Color.fromARGB(255, 95, 128, 236),
+                ]))
+            // filled: true,
+            // fillColor: Colors.white.withOpacity(0.3),
+            ),
       ),
     );
   }
