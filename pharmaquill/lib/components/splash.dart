9073255670ffea1a577a53eb514ef621da0e0a68
Madhurjya Dasgupta <madhurjya2003@icloud.com>
2024-02-17 17:48:06 +0000

    Changed colors

--- pharmaquill/lib/components/splash.dart
+++ pharmaquill/lib/components/splash.dart
@@ -6,7 +6,7 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 106, 168, 218),
+      backgroundColor: const Color.fromRGBO(0, 36, 107, 1),
       body: Column(
         children: [
           Expanded(
@@ -14,31 +14,39 @@ class SplashPage extends StatelessWidget {
             child: Container(),
           ),
           SizedBox(
+            height: 50,
+            width: 150,
             child: ElevatedButton(
               style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.yellow,
+                  backgroundColor: const Color.fromRGBO(202, 220, 252, 1),
                   shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(30))),
               onPressed: () => print("Hello World"),
               child: const Text(
-                "Log In",
+                "Login",
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white, fontSize: 30),
+              ),
+            ),
+          ),
+          const Padding(padding: EdgeInsets.all(5)),
+          SizedBox(
+            height: 50,
+            width: 150,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromRGBO(204, 49, 61, 1),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30))),
+              onPressed: () => Navigator.pushNamed(context, '/signup'),
+              child: const Text(
+                "Sign Up",
                 textAlign: TextAlign.center,
                 style: TextStyle(color: Colors.white),
               ),
             ),
           ),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-                backgroundColor: const Color.fromARGB(255, 255, 17, 0),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30))),
-            onPressed: () => Navigator.pushNamed(context, '/signup'),
-            child: const Text(
-              "Sign Up",
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.white),
-            ),
-          ),
+          const Padding(padding: EdgeInsets.only(bottom: 10)),
         ],
       ),
     );
