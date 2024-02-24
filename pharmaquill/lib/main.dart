15f7a664406a41a005400946e1fdf274d5aaf801
Madhurjya Dasgupta <madhurjya2003@icloud.com>
2024-02-24 07:11:41 +0000

    Use Material3

--- pharmaquill/lib/main.dart
+++ pharmaquill/lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
       home: const SplashPage(),
       routes: {
         '/splash': (context) => const SplashPage(),
