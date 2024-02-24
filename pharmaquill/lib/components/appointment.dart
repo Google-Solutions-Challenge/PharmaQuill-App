53ed9725e302ffcb3a7b9fa2f2309ed33ee5ba2d
Madhurjya Dasgupta <madhurjya2003@icloud.com>
2024-02-24 07:12:08 +0000

    Created Appointment Screen

--- pharmaquill/lib/components/appointment.dart
+++ pharmaquill/lib/components/appointment.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+
+void main() => runApp(const Appointment());
 
 class Appointment extends StatefulWidget {
   const Appointment({super.key});
@@ -9,43 +10,146 @@ class Appointment extends StatefulWidget {
 }
 
 class _AppointmentState extends State<Appointment> {
+  bool isDark = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          leading: const BackButton(color: Colors.white),
-          backgroundColor: const Color.fromRGBO(3, 4, 94, 1),
-          title: const Text(
-            "Appointment",
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 30, color: Colors.white),
-          ),
-        ),
+      backgroundColor: const Color.fromRGBO(3, 4, 94, 1),
+      appBar: AppBar(
         backgroundColor: const Color.fromRGBO(3, 4, 94, 1),
-        body: Column(
-          children: [
-            Row(
-              children: [
-                Text(
-                  "Find Your Specialist",
-                  style: GoogleFonts.lato(
-                    textStyle: const TextStyle(
-                        color: Colors.blue, letterSpacing: .5, fontSize: 30),
-                  ),
+        title: const Text(
+          'Appointments',
+          style: TextStyle(color: Colors.white),
+        ),
+        leading: const BackButton(
+          color: Colors.white,
+        ),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SearchAnchor(
+                builder: (BuildContext context, SearchController controller) {
+              return SearchBar(
+                  controller: controller,
+                  padding: const MaterialStatePropertyAll<EdgeInsets>(
+                      EdgeInsets.symmetric(horizontal: 16.0)),
+                  onTap: () {
+                    controller.openView();
+                  },
+                  onChanged: (_) {
+                    controller.openView();
+                  },
+                  leading: const Icon(Icons.search),
+                  trailing: const <Widget>[Icon(Icons.filter_alt_outlined)]);
+            }, suggestionsBuilder:
+                    (BuildContext context, SearchController controller) {
+              return List<ListTile>.generate(5, (int index) {
+                final String item = 'Doctor $index';
+                return ListTile(
+                  title: Text(item),
+                  onTap: () {
+                    setState(() {
+                      controller.closeView(item);
+                    });
+                  },
+                );
+              });
+            }),
+          ),
+          Row(
+            children: [
+              const Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "Top Doctors",
+                  style: TextStyle(color: Colors.white, fontSize: 30),
                 ),
-                const Padding(padding: EdgeInsets.only(left: 70)),
-                const Icon(
-                  Icons.person,
-                  size: 40,
-                  color: Colors.white,
-                )
-              ],
+              ),
+              const Spacer(),
+              Align(
+                alignment: Alignment.topRight,
+                child: InkWell(
+                  child: const Text(
+                    "See All",
+                    style: TextStyle(color: Colors.lightGreen, fontSize: 20),
+                  ),
+                  onTap: () => print("Hello World"),
+                ),
+              )
+            ],
+          ),
+          DoctorList()
+        ],
+      ),
+    );
+  }
+}
+
+class DoctorList extends StatelessWidget {
+  final List<Map<String, String>> doctors = [
+    {
+      'name': 'Dr. Jane Doe',
+      'specialty': 'Anonymous Pharmacy, Majhitar',
+      'time': '10:30 AM - 3:00 PM',
+      'fee': '800INR'
+    },
+    {
+      'name': 'Dr. Jane Doe',
+      'specialty': 'Anonymous Pharmacy, Majhitar',
+      'time': '10:30 AM - 3:00 PM',
+      'fee': '800INR'
+    },
+    {
+      'name': 'Dr. Jane Doe',
+      'specialty': 'Anonymous Pharmacy, Majhitar',
+      'time': '10:30 AM - 5:00 PM',
+      'fee': '600INR'
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: doctors.length,
+      itemBuilder: (context, index) {
+        var doctor = doctors[index];
+        return Padding(
+          padding: const EdgeInsets.all(5),
+          child: Card(
+            child: ListTile(
+              shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10))),
+              tileColor: Colors.blueAccent,
+              leading: const Icon(
+                Icons.person,
+                color: Colors.white,
+              ), // Replace with actual image
+              title: Text(
+                doctor['name']!,
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(doctor['specialty']!),
+                  Text(doctor['time']!),
+                  Text('Fee: ${doctor['fee']}'),
+                ],
+              ),
+              trailing: ElevatedButton(
+                onPressed: () {
+                  print("Button pressed");
+                },
+                child: const Text('Appointment'),
+              ),
             ),
-            const SearchBar(
-              padding: MaterialStatePropertyAll<EdgeInsets>(
-                  EdgeInsets.symmetric(horizontal: 16.0)),
-            )
-          ],
-        ));
+          ),
+        );
+      },
+    );
   }
 }
